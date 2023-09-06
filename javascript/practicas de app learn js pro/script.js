f066d2e9fe142a36d22f81a78635af1e9c84207d
Rafael <mg.rafaeld@gmail.com>
2023-09-06 12:59:07 -0300

    comienzo de pruebas dre javascript

---
+++ javascript/practicas de app learn js pro/script.js
@@ -0,0 +1,5 @@
+document.getElementById("elem").innerHTML = "Esta es una prueba de '.innerhtml'. De esta forma capaz puedo dominar los cambios de los contenidos de HTML con el fin de proteger mi codigo de copycats";
+
+document.write("Hola esto es otro ejemplo de entrada de info a HTML por JS")
+
+alert("esto es una alerta")
