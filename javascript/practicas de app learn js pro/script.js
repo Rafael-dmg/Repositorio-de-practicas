22608e1809ce8b907ec8e4c14debf94e2ce6423b
Rafael <mg.rafaeld@gmail.com>
2023-09-07 11:19:43 -0300

    principio de practicas de javascript learnpro

--- javascript/practicas de app learn js pro/script.js
+++ javascript/practicas de app learn js pro/script.js
@@ -1,5 +1,9 @@
+'use strict';
+
 document.getElementById("elem").innerHTML = "Esta es una prueba de '.innerhtml'. De esta forma capaz puedo dominar los cambios de los contenidos de HTML con el fin de proteger mi codigo de copycats";
 
 document.write("Hola esto es otro ejemplo de entrada de info a HTML por JS")
 
-alert("esto es una alerta")
+function alertFunc() {
+    alert("esto es una alerta")
+}
