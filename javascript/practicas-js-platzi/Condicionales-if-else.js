8652ca1b4d06246ad1d1d628b3d8250ca52ef18d
Ram-Gad <mg.rafaeld@gmail.com>
2023-10-07 01:23:37 -0300

    creados archivos en carpeta javascript

---
+++ javascript/practicas-js-platzi/Condicionales-if-else.js
@@ -0,0 +1,20 @@
+var edad = 18
+
+if (edad === 18) {
+    console.log("Felicidades puedes votar por primera vez")
+} else if (edad > 18) {
+    console.log("Puedes votar nuevamente")
+} else if (edad < 18) {
+    console.log("sabes que aun no puedes votar larva")
+};
+
+//----------------------------------------------------------------------
+
+
+//Operador ternario
+
+//ejm: condition ? true:false
+
+let numero = 2;
+
+let resultado = numero === 1 ? "si soy un uno" : "no no soy un uno"
---
+++ javascript/practicas-js-platzi/switch.js
@@ -0,0 +1,3 @@
+//switch es otro condicional pero este validara por casos, si el caso es verdad, pasara la condicion indicada, en caso contrario pasara a la siguiente condicion o si no dara un resultado final. 
+
+
