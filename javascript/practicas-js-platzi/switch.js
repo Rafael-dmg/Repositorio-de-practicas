552cdd0d5463f47b84b3c68edd824c763e9641b3
Ram-Gad <mg.rafaeld@gmail.com>
2023-10-07 01:58:36 -0300

    clase de switch

--- javascript/practicas-js-platzi/switch.js
+++ javascript/practicas-js-platzi/switch.js
@@ -1,3 +1,24 @@
 //switch es otro condicional pero este validara por casos, si el caso es verdad, pasara la condicion indicada, en caso contrario pasara a la siguiente condicion o si no dara un resultado final. 
+var numero = 1;
 
+switch (true) {
+    case 1:
+        console.log("Soy uno!")
+        break;
+
+    case 10:
+        console.log("Soy un diez!")
+        break;
+
+    case 100:
+        console.log("Soy un cien!")
+        break;
+
+    default:
+        console.log("No soy nada")
+}
+
+//si no colocamos los breaks, realiza todas las validaciones que le indiquemos, asi que son fundamentales
+
+//RETO piedra, papel o tijera:
 
