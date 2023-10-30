e227c0815e272e09a858e7d1af1bcc4fe6b27b1b
Ram-Gad <mg.rafaeld@gmail.com>
2023-10-30 13:02:46 -0300

    arrays

--- javascript/practicas-js-platzi/arrays.js
+++ javascript/practicas-js-platzi/arrays.js
@@ -1,4 +1,37 @@
-let acordes = ["do", "re", "mi", "fa"]
+let acordes = ["do", "re", "mi", "fa"] //DATO IMPORTANTE: Un array no guarda 'datos distintos' por ejemplo: no puede guardar numeros y strings en un mismo array
+
+console.log(acordes)//para ver el contenido
+
+console.log(acordes.length)//para ver la longitud del array
+
+console.log(acordes[2])//de esta manera buscamos una posicion especifica del array. PD: en prog. se cuenta desde el 0
+
+//como agregamos mas elementos al array? con el metodo .push
+var masAcordes = acordes.push('sol')//agrega un elemento al ultimo lugar de la lista, si o si
+
+var menosAcordes = acordes.pop('sol')//este motodo elimina el ULTIMO elemento del array
+
+var nuevoComienzo = acordes.unshift('si')//este metodo agrega un elemento a la lista pero lo agrega en la primera posicion
+
+var borrarAcorde = acordes.shift('si')//elimina el primer elemento del array
+
+//INDEXOF - nos permite buscar la posicion de los elementos en el array, por ejemplo si queremos saber la posicion del acorde fa de nuestro ejemplo
+
+var posicion = acordes.indexOf('fa')//busca la posicion del elemento que le indiquemos en el array
+
+posicion//se debe llamar luego a la variable para poder saber el resultado de la posicion del array
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
