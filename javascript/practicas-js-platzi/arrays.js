55dbff36844c19d423b565e9a7f65f8f111b438d
Rafael <mg.rafaeld@gmail.com>
2023-10-31 23:05:45 -0300

    ejercicio de array repasado

--- javascript/practicas-js-platzi/arrays.js
+++ javascript/practicas-js-platzi/arrays.js
@@ -21,8 +21,21 @@ var posicion = acordes.indexOf('fa')//busca la posicion del elemento que le indi
 
 posicion//se debe llamar luego a la variable para poder saber el resultado de la posicion del array
 
+// --------------------------------------------------------------------------------------------------------------
 
+// En este desafío vas a recibir un parámetro arraySecreto en la función solution. Debes retornar true si el primer elemento del arraySecreto es de tipo string y debes retornar false si es de cualquier otro tipo.
 
+// Input
+
+// solution(["Huevo", "Gallina", "Vaca"])
+// solution([1, "Gallina", "Vaca"])
+
+// Output
+
+// true
+// false
+
+// ----------------------------------------------------------------------------------------------------------------
 
 
 function solution(arraySecreto) {
