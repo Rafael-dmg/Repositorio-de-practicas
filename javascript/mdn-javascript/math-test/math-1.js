a3b7c061a87d53ff7c0eb1718ff554feef5dbcef
Ram-Gad <mg.rafaeld@gmail.com>
2023-11-14 12:24:17 -0300

    avance en tarea de javascript MDN

---
+++ javascript/mdn-javascript/math-test/math-1.js
@@ -0,0 +1,38 @@
+let finalResult;
+
+    let evenOddResult;
+
+    // Add your code here
+    let num1 = 2
+    let num2 = 4
+    let num3 = 8
+    let num4 = 16
+
+function operation (numx, numy) {
+   var numx = num2 + num1;
+   var numy = num4 - num3;
+
+finalResult = numx * numy;
+
+evenOddResult = finalResult;
+
+return evenOddResult % 2 === 0 ? true : false;
+
+}
+
+operation()
+
+
+    // Don't edit the code below here!
+
+    const section = document.querySelector('section');
+
+    const para1 = document.createElement('p');
+    const finalResultCheck = finalResult === 48 ? `Yes, well done!` : `No, it is ${ finalResult }`;
+    para1.textContent = `Is the finalResult 48? ${ finalResultCheck }`;
+    const para2 = document.createElement('p');
+    const evenOddResultCheck = evenOddResult === 0 ? 'The final result is even!' : 'The final result is odd. Hrm.';
+    para2.textContent = evenOddResultCheck;
+
+    section.appendChild(para1);
+    section.appendChild(para2);
