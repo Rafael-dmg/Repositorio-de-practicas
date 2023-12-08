6fa4de3ae0709ce3c0b503373c9dce66a8bf124a
Rafael <mg.rafaeld@gmail.com>
2023-12-08 12:35:39 -0300

    strings

---
+++ javascript/javascriptDesdeCero/Manipulacion de strings/script.js
@@ -0,0 +1,15 @@
+//string primitivos
+
+const stringPrimitivo = 'Soy un string primitivo';
+console.log(typeof stringPrimitivo);
+
+const stringPrimitivoTambien = String('Tambien soy un string primitivo');
+console.log(typeof stringPrimitivoTambien);
+
+//String objeto
+
+const cadenaObjeto = new String('Soy un string objeto papu');
+console.log(typeof cadenaObjeto);
+
+//Acceder a caracteres
+
