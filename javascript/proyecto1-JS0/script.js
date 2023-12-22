548857aa1bedf51649ffbc8947be242308f616e3
Rafael <mg.rafaeld@gmail.com>
2023-12-22 00:04:10 -0300

    avances en proyecto1 de JS 0

--- javascript/proyecto1-JS0/script.js
+++ javascript/proyecto1-JS0/script.js
@@ -1,5 +1,27 @@
-let userName = '';
-let fullName = '';
-let age = '';
+let userName = 'Ramgad';
+let fullName = 'Rafael Martinez';
+let age = '32';
 let isStudent = true;
+
+let address = {
+    street: 'Evergreen',
+    city: 'Springfield',
+    state: 'Whereville',
+    zipCode: '4523'
+}
+
+let hobbies = ['coding,', 'play the guitar,', 'learning languages']
+let complementary = 'and';
+
+let completeSentence = hobbies.slice(0, 2).concat(complementary).concat(hobbies.slice(2));
+
+let bio = `Hello and welcome to my blog, my name is ${fullName},
+i'm ${age} years old.
+My favorite activities are ${completeSentence.join(' ')}. 
+Follow me to learn some more coding hacks, you can also contact me on ${userName}@tumail.com`
+
+
+console.log(bio)
+
+let infoTexto = document.querySelector('#info').innerHTML = bio;
 
