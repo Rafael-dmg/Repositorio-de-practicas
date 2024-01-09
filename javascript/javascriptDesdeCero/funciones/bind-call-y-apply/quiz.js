e43f5b6c6717be1d0a79c78e5f8fa7ff68114fdf
Ram-Gad <mg.rafaeld@gmail.com>
2024-01-09 12:55:10 -0300

    terminado con exito el quiz

--- javascript/javascriptDesdeCero/funciones/bind-call-y-apply/quiz.js
+++ javascript/javascriptDesdeCero/funciones/bind-call-y-apply/quiz.js
@@ -1,11 +1,22 @@
 const caricatura = {
-    nombre:'Coraje el perro cobarde'
+    nombre:'Coraje el perro cobarde',
+    duenos: ['Coraje', 'Muriel', 'Justo']
 }
 
-function recuerdo () {
-    console.log(`${this.nombre} era mi caicatura favorita cuando era niño. 
+function recuerdo (personaje1, personaje2, personaje3) {
+    console.log(`${this.nombre} era mi caricatura favorita cuando era niño. 
     Podia pasar horas viendo las aventuras y desventuras de ${personaje1}, ${personaje2} y ${personaje3}`)
 }
 
-recuerdo.call(caricatura, 'Coraje')
-recuerdo.bind(caricatura, 'Muriel')
+
+
+recuerdo.call(caricatura, 'Coraje', 'Muriel', 'Justo')
+// recuerdo.bind(caricatura, 'Muriel')
+
+let bindingMemories = recuerdo.bind(caricatura, 'Coraje', 'Muriel', 'Justo')
+
+bindingMemories()
+
+recuerdo.apply(caricatura, caricatura.duenos)
+
+
