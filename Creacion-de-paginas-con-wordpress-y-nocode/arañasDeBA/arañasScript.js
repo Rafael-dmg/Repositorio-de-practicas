b4532c159b7b00ee76ddf09811d343e7b21c5afc
Rafael <mg.rafaeld@gmail.com>
2024-02-04 19:58:59 -0300

    practica de jumbotron

--- Creacion-de-paginas-con-wordpress-y-nocode/arañasDeBA/arañasScript.js
+++ Creacion-de-paginas-con-wordpress-y-nocode/arañasDeBA/arañasScript.js
@@ -7,6 +7,8 @@ const hero = document.querySelector('#logo')
 
 const arañaBlanca = document.createElement('img')
 arañaBlanca.setAttribute('src', 'https://i.pinimg.com/originals/0c/b3/84/0cb3840a4fae42af6b982bed26b38a7f.jpg')
+arañaBlanca.setAttribute('class', 'arañaBlanca')
+
 
 const arañaLobo = document.createElement('img')
 arañaLobo.setAttribute('src','../assets/arañaLobo.png')
