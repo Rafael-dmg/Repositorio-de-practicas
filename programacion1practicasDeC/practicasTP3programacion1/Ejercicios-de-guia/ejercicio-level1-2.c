e0742842398a536723a71b325d6115112cb18e70
Ram-Gad <mg.rafaeld@gmail.com>
2024-05-09 00:28:27 -0300

    inventems

---
+++ programacion1practicasDeC/practicasTP3programacion1/Ejercicios-de-guia/ejercicio-level1-2.c
@@ -0,0 +1,7 @@
+/* En una sala de juegos existen 4 salas: Consolas, Juegos2D, Juegos 3D y VR. 
+Si un usuario paga 4 creditos o mas puede acceder a todas, si paga solo 3 podra acceder a las tres 
+primeras. Si paga 2 podra acceder a las 2 primeras y si paga 1 solo a la primera sala. Ingresar por teclado
+los creditos y mostrar a que juegos puede acceder. */
+
+#include <stdio.h>
+
