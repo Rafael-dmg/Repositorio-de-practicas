6c7c5021ff2df6681146260da86c0ada65218e63
Ram-Gad <mg.rafaeld@gmail.com>
2024-05-08 23:44:41 -0300

    avance con el lenguaje C, sintaxis, condicionales y mas

---
+++ programacion1practicasDeC/practicasTP3programacion1/Ejercicios-de-guia/ejercicio-level1-1.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+
+int eleccion, cuadrado, rectangulo, circulo;
+float lado, perimetro, base, altura, area, radio;
+
+int main ()
+{
+    printf("Ingrese un numero para elegir la forma geometrica\n");
+    scanf("%d", &eleccion);
+
+    switch(eleccion) 
+    {
+        case 0:
+            printf("Ingresa una opcion valida");
+            break;
+
+        case 1:
+           printf("Elegiste cuadrado\n");
+            printf("Ingresa el lado de un cuadrado para saber su area y su perimetro\n");
+            scanf("%f", &lado);
+
+            if (lado > 0) {
+                area = lado * lado;
+                perimetro = area *2;
+                printf("Area: %.1f\n", area);
+                printf("Perimetro: %.1f\n", perimetro);
+            }   
+                else
+                    printf("Ingrese un numero mayor a 0\n");
+        break;
+
+        case 2:
+            printf("Elegiste rectangulo\n");
+            printf("Ingresar los valores para calcular area y perimetro\n");
+            printf("Ingrese la base del rectangulo: \n");
+            scanf("%f", &base);
+            printf("Ingrese la altura del rectangulo: \n");
+            scanf("%f", &altura);
+
+            area = base * altura;
+            perimetro = 2 * (base + altura);
+            printf("Area: %.2f\n", area);
+            printf("Perimetro: %.2f\n", perimetro);
+            break;
+
+        case 3:
+            printf("Elegiste circulo\n");
+            printf("Ingresar los valores para calcular area y perimetro\n");
+            printf("Ingrese el radio del circulo\n");
+            scanf("%f", &radio);
+
+            area = 3.14 * (radio * radio);
+            perimetro = (3.14 * 2) * radio;
+            printf("Area: %.2f\n", area);
+            printf("Perimetro: %.2f\n", perimetro);
+            break;
+    }
+}
---
+++ programacion1practicasDeC/practicasTP3programacion1/condicionales/condicional-switch.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+// #define P printf
+// #define S scanf
+
+int dia;
+
+int main ()
+{
+printf("Ingrese un numero");
+scanf("%d", &dia);
+
+switch(dia)
+{
+    case 1:
+        printf("Lunes");
+        break;
+
+        default:
+            printf("no hay otro dia chau");
+}
+
+}
+
+
