adb6406d859f53a6fec077bf2243e547c2b2fd8b
Ram-Gad <mg.rafaeld@gmail.com>
2024-05-15 08:30:56 -0300

    movimientos de ciertos archivos de prog

--- programacion1practicasDeC/practicasTP3programacion1/Ejercicios-de-guia/Condicionales/Switch/ejercicio-level1-2.c
+++ programacion1practicasDeC/practicasTP3programacion1/Ejercicios-de-guia/Condicionales/Switch/ejercicio-level1-2.c
@@ -4,3 +4,56 @@ primeras. Si paga 2 podra acceder a las 2 primeras y si paga 1 solo a la primera
 los creditos y mostrar a que juegos puede acceder. */
 
 #include <stdio.h>
+
+int creditos;
+char irOquedarse;
+
+int main ()
+{
+    printf("Cuantos creditos quiere comprar?\n");
+    printf("1 Credito para Consolas\n");
+    printf("2 Creditos para Consolas + Juegos 2D\n");
+    printf("3 Creditos para Consolas + Juegos 2D + Juegos 3D\n");
+    printf("4 Creditos para Consolas + Juegos 2D + Juegos 3D + Realidad Virtual\n");
+    printf("Cuantos Creditos vas a comprar hoy?:  ");
+    scanf("%d", &creditos);
+
+    if (creditos == 0)
+    {
+        printf("Estas seguro? tenemos buenos juegos\n");
+        printf("Ingresa Y para elegir jugar o N para elegir aburrirte: \n");
+        scanf(" %c", &irOquedarse);
+
+        if (irOquedarse == 'y')
+            printf("Veo que eres una persona divertida\n");
+        else if (irOquedarse == 'n')
+            printf("Sera para la proxima, adios\n");
+    }
+    else if (creditos < 1 || creditos > 4)
+        printf("Ingresa un valor correcto de creditos\n");    
+
+    else if (creditos > 1 || creditos < 4) 
+    {
+        switch (creditos)
+        {
+            case 1: 
+                printf("Excelente! puedes acceder a la sala: Consolas!\n");
+                break;
+
+            case 2:
+                printf("Excelente! puedes acceder a las salas: Consolas, Juegos 2D!\n");
+                break;
+
+            case 3:
+                printf("Excelente! puedes acceder a las salas: Consolas, Juegos 2D y Juegos 3D!\n");
+                break;
+
+            case 4:
+                printf("Excelente! puedes acceder a la experiencia completa de Consolas, Juegos 2D, Juegos 3D y Realidad virtual!\n");
+                break;
+                
+        }
+    }    
+
+    return 0; 
+}
