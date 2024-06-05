90fdd4f04a606cdaac215b4c07e467795394dc3d
Rafael <mg.rafaeld@gmail.com>
2024-06-05 13:01:41 -0300

    terminado ejercicio de factorial modulado

--- informaticaAplicada/programacion1practicasDeC/practicasTP4programacion1/while/tp4ejercicio1.c
+++ informaticaAplicada/programacion1practicasDeC/practicasTP4programacion1/while/tp4ejercicio1.c
@@ -4,12 +4,15 @@
 #define P printf
 #define S scanf
 
+void holaMundo (void);
+void cargarDatos(int*);
+
 int main ()
 {
   int num, rest = 1;
 
-  P("Ingrese un valor para obtener su factorial\n");
-  S("%d", &num);
+  cargarDatos(&num);
+
 
   for(int i = num; i > 0; i--)
   {
@@ -18,7 +21,16 @@ int main ()
     P("valor de i %d\n", i);
     P("El valor del factorial es: %d\n", rest);
   }
-    
+    holaMundo();
 }
 
+void holaMundo (void)
+{
+  printf("Hola, mundo\n");
+}
 
+void cargarDatos(int* num)
+{
+    P("Ingrese un valor para obtener su factorial\n");
+    S("%d", num);
+}
