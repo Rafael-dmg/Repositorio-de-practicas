590e5b930567e3adbb32d5c45aa15607e2a4c571
Rafael <mg.rafaeld@gmail.com>
2024-06-15 19:04:37 -0300

    de vuelta a la version autocorregida del examen

--- informaticaAplicada/parcial05-06-24/Rafael_Martinez.c
+++ informaticaAplicada/parcial05-06-24/Rafael_Martinez.c
@@ -3,80 +3,80 @@
 #define S scanf
 
 int mostrarDatos(int, int, int);
-int registrarAsistencia(int);
+void registrarAsistencia(int*);
 void mostrarEstadisticas(int, int, int);
-int areaMasConcurrida(int, int, int, int, int, int);
+int areaMasConcurrida(int, int, int, int*, int*, int*);
 int menu(void);
 
 int main (void)
 {
-  int biblioteca, laboratorio, aula, elec, mayor, medio, menor;
-  float promedio;
-
-  int op;
+  int op, biblioteca, laboratorio, aula, elec, mayor, medio, menor, alumBiblioteca  = 50, alumLaboratorio = 50, alumAula = 50;
+  float promedio = 0;
 
   do 
   {
     op =menu();
     switch(op)
     {
-      case 1:
-        do
-        {
-          registrarAsistencia(elec);
-        } while (elec != 0);
-        switch (elec)
-        {
         case 1:
-            do
-            {
-                P("Cargue alumnos de Laboratorio\n");
-                S("%d", biblioteca);
-            } while (biblioteca <= 0);
-              break;
-        case 2:
-
-            do
+          do
           {
-              P("Cargue alumnos de Biblioteca\n");
-              S("%d", laboratorio);
-          } while (laboratorio <= 0);
-        case 3:
+            registrarAsistencia(&elec);
+          
+            switch (elec)
+            {
+            case 1:
+                do
+                {
+                    P("Cargue alumnos de Laboratorio\n");
+                    S("%d", &biblioteca);
+                } while (biblioteca <= 0);
+                  break;
 
-            do
-        {
-            P("Cargue alumnos de Aula\n");
-            S("%d", aula);
-        } while (aula <= 0);
-          break;
-        }
+              case 2:
+                  do
+                {
+                    P("Cargue alumnos de Biblioteca\n");
+                    S("%d", &laboratorio);
+                } while (laboratorio <= 0);
+                break;
+
+              case 3:
+                  do
+              {
+                  P("Cargue alumnos de Aula\n");
+                  S("%d", &aula);
+              } while (aula <= 0);
+                break;
+            
+            }
+
+          } while (elec != 0);
           break; 
 
       case 2:
-          
-          mostrarDatos(biblioteca, laboratorio, aula);
-          break; 
+            
+            mostrarDatos(biblioteca, laboratorio, aula);
+            break; 
 
       case 3:
-          mostrarEstadisticas (biblioteca, laboratorio, aula);
-          P("El promedio es de: %.2f\\n", promedio);
-          break; 
+            mostrarEstadisticas (biblioteca, laboratorio, aula);
+            break; 
 
       case 4:
-        areaMasConcurrida(biblioteca, laboratorio, aula, mayor, medio, menor);
-          break; 
+          areaMasConcurrida(biblioteca, laboratorio, aula, &mayor, &medio, &menor);
+            break; 
 
       case 5: 
-          break; 
+            break; 
+     }
 
-    }
-
-  } while (op);
+} while (op != 0);
 
   return 0;
 }
 
-menu(void)
+int menu (void)
 {
   int op;
 
@@ -89,19 +89,19 @@ menu(void)
   P("5.- Encontrar %crea menos concurrida. \n", 160);
   P("0.- SALIR\n");
   P("INGRESE OPCI%cN: \n", 224);
-  S("%d\n", &op);
+  S("%d", &op);
 
   return op;
 }
 
-int registrarAsistencia(int elec)
+void registrarAsistencia(int *elec)
 {
 
-  P("Ingrese el area que desea cargar");
-  P("1.- Para Laboratorio");
-  P("2.- Para Biblioteca");
-  P("3.- Para Aula");
-  P("0.- Salir");
+  P("Ingrese el area que desea cargar\n");
+  P("1.- Para Laboratorio\n");
+  P("2.- Para Biblioteca\n");
+  P("3.- Para Aula\n");
+  P("0.- Salir\n");
   S("%d", elec);
 }
 
@@ -111,32 +111,69 @@ int mostrarDatos(int biblioteca, int laboratorio, int aula)
   P("Laboratorio: %d\n", biblioteca);
   P("Biblioteca: %d\n", laboratorio);
   P("Aula: %d\n", aula);
+
+  return 0;
 }
 
 void mostrarEstadisticas(int biblioteca, int laboratorio, int aula)
 {
-  int promedio; 
 
-  promedio = (biblioteca + laboratorio + aula) / 3;
+ float promedio = (biblioteca + laboratorio + aula) / 3;
+ P("El promedio de alumnos es: %.2f\n", promedio);
 }
 
-int areaMasConcurrida(int biblioteca, int laboratorio, int aula, int mayor, int medio, int menor);
+int areaMasConcurrida(int biblioteca, int laboratorio, int aula, int *mayor, int *medio, int *menor)
 {
-  mostrarDatos( biblioteca, laboratorio, aula)
+    mostrarDatos(biblioteca, laboratorio, aula);
   
-  if(biblioteca > laboratiorio && biblioteca > aula)
-  {
-    mayor = biblioteca;
-      if(laboratorio > aula)
-      {
-        medio = laboratorio;
-        menor = aula
-      }else
-      {
-        medio = aula;
-        menor = laboratorio;
-      }
-  }
+    if (biblioteca > laboratorio && biblioteca > aula)
+    {
+        *mayor = biblioteca;
+        if (laboratorio > aula)
+        {
+            *medio = laboratorio;
+            *menor = aula;
+        }
+        else
+        {
+            *medio = aula;
+            *menor = laboratorio;
+        }
+    }
+    else if (laboratorio > biblioteca && laboratorio > aula)
+    {
+        *mayor = laboratorio;
+        if (biblioteca > aula)
+        {
+            *medio = biblioteca;
+            *menor = aula;
+        }
+        else
+        {
+            *medio = aula;
+            *menor = biblioteca;
+        }
+    }
+    else
+    {
+        *mayor = aula;
+        if (biblioteca > laboratorio)
+        {
+            *medio = biblioteca;
+            *menor = laboratorio;
+        }
+        else
+        {
+            *medio = laboratorio;
+            *menor = biblioteca;
+        }
+    }
+
+    P("Mayor concurrencia: %d\n", *mayor);
+    P("Concurrencia media: %d\n", *medio);
+    P("Menor concurrencia: %d\n", *menor);
+
+    return 0;
     
 }
         
