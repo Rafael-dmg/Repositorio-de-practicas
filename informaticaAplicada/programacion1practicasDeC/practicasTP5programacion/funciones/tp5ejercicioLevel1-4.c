f9fdcd96a46d0849af4b3082f2f6b6a7ca04e3c0
Rafael <mg.rafaeld@gmail.com>
2024-06-26 12:57:05 -0300

    avanzando con ejercicio de programacion de login

--- informaticaAplicada/programacion1practicasDeC/practicasTP5programacion/funciones/tp5ejercicioLevel1-4.c
+++ informaticaAplicada/programacion1practicasDeC/practicasTP5programacion/funciones/tp5ejercicioLevel1-4.c
@@ -2,3 +2,35 @@
 #define P printf
 #define S scanf 
 
+int login (int*, char*);
+
+int main(void)
+{
+  int dni, intentos = 0;
+  char passw;
+
+do
+{
+  login(&dni, &passw);
+
+  if (dni != 19083959)
+  {
+    P("error\n");
+    intentos += 1;
+  }
+
+} while (intentos != 3);
+
+  
+  
+}
+
+int login (int *dni, char *passw)
+{
+  P("Ingrese su documento\n");
+  S("%d", dni);
+  P("Ingrese password\n");
+  S("%s", passw);
+
+  return 0;
+}
