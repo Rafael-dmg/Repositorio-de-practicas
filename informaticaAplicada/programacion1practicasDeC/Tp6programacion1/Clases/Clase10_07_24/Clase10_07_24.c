33470bc49c694ccaf5e00ef808368b02a64778ec
Rafael <mg.rafaeld@gmail.com>
2024-07-10 21:22:08 -0300

    avanzado con el ejercicio de clase

--- informaticaAplicada/programacion1practicasDeC/Tp6programacion1/Clases/Clase10_07_24/Clase10_07_24.c
+++ informaticaAplicada/programacion1practicasDeC/Tp6programacion1/Clases/Clase10_07_24/Clase10_07_24.c
@@ -1,3 +1,64 @@
+// #include <stdio.h>
+// #include <stdlib.h>
+// #include <time.h>
+// #include <string.h>
+// #include <ctype.h>
+// #include <math.h>
+// #define N 10
+// void CargarVector(int [N]);
+// void mostrarVector(int [N],char *);
+// void ordenarVector(int [N], int);
+// int main()
+// {
+//     int vec[N]; // definir o declarar un vector
+//     CargarVector(vec);
+//     mostrarVector(vec, "Vector Cargado SIN ordenar");
+//     ordenarVector(vec,N);
+//     mostrarVector(vec, "Vector ORDENADO");
+//     return 0;
+// }
+// void CargarVector(int v[N])
+// {
+//     int i=0;
+//     do
+//     {
+//         printf("\n Ingrese un dato <%2d> de <%2d>:",i+1,N);
+//         scanf("%d",&v[i]);
+//         i++;
+//     } while (i<N);
+    
+// }
+// void mostrarVector(int x[N], char *tit)
+// {
+//     int i;
+//     printf("\n%s\n",tit);
+//     for(i=0;i<N;i++)
+//     {
+//         printf("%3d\n",x[i]);
+//     }
+// }
+// void ordenarVector(int x[], int y)
+// {
+//     // y posse laa cantidad de valores a Ordenar
+//     int i, cota = y - 1, k = 1, aux; // k es una bandera, que me indica que se termino de ordenar
+//     while (k)
+//     {
+//         k = 0; // partimos del supuesto que esta ordenado
+//         for (i = 0; i < cota; i++)
+//         {
+//             if (x[i] > x[i + 1])
+//             {
+//                 // aqui se produce el swap
+//                 aux = x[i];
+//                 x[i] = x[i + 1];
+//                 x[i + 1] = aux;
+//                 k = i; // aqui k toma el valor donde fue ordenado por ultima vez
+//             }
+//         }
+//         cota = k; // aqui se lo pasa a cota, con lo cual lo limita en las iteracioners
+//     }
+// }
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
@@ -5,56 +66,41 @@
 #include <ctype.h>
 #include <math.h>
 #define N 10
-void CargarVector(int [N]);
-void mostrarVector(int [N],char *);
-void ordenarVector(int [N], int);
+char Nombres[N][30]; //los primeros corchetes serian las 'filas' y el segundo corchete serian las 'filas'
+
+void CargarVector(char[][30]);
+void OrdenarAscendente(char[][30], int);
+void OrdenarDescendente(char[][30], int);
+void MostrarVector(char[][30], char *);
+
 int main()
 {
-    int vec[N]; // definir o declarar un vector
-    CargarVector(vec);
-    mostrarVector(vec, "Vector Cargado SIN ordenar");
-    ordenarVector(vec,N);
-    mostrarVector(vec, "Vector ORDENADO");
-    return 0;
+  char nombre[25];
+  
+  CargarVector(nombre);
+  MostrarVector(nombre, "mensajito jo jo");
 }
-void CargarVector(int v[N])
+char nombre[25];
+
+void CargarVector(char nombre[][30])
 {
-    int i=0;
+  int i=0;
     do
     {
         printf("\n Ingrese un dato <%2d> de <%2d>:",i+1,N);
-        scanf("%d",&v[i]);
+        scanf("%s",&nombre[i]);
         i++;
     } while (i<N);
-    
+  
 }
-void mostrarVector(int x[N], char *tit)
+
+void MostrarVector(char nombre[][30], char *msj)
 {
-    int i;
-    printf("\n%s\n",tit);
-    for(i=0;i<N;i++)
-    {
-        printf("%3d\n",x[i]);
-    }
-}
-void ordenarVector(int x[], int y)
-{
-    // y posse laa cantidad de valores a Ordenar
-    int i, cota = y - 1, k = 1, aux; // k es una bandera, que me indica que se termino de ordenar
-    while (k)
-    {
-        k = 0; // partimos del supuesto que esta ordenado
-        for (i = 0; i < cota; i++)
-        {
-            if (x[i] > x[i + 1])
-            {
-                // aqui se produce el swap
-                aux = x[i];
-                x[i] = x[i + 1];
-                x[i + 1] = aux;
-                k = i; // aqui k toma el valor donde fue ordenado por ultima vez
-            }
-        }
-        cota = k; // aqui se lo pasa a cota, con lo cual lo limita en las iteracioners
-    }
+  int i;
+
+  for (i = 0; i < N; i++)
+  {
+    printf("\n%s\n", nombre[i]);
+  }
+  
 }
