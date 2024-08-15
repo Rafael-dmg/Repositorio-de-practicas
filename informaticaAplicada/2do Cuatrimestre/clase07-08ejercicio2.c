bf02524d975a5548bd44f0e43f1927ee12b5d64f
Rafael <mg.rafaeld@gmail.com>
2024-08-15 20:49:05 -0300

    terminada clase 07/8 - comenzando a entender matrices

--- informaticaAplicada/2do Cuatrimestre/clase07-08ejercicio2.c
+++ informaticaAplicada/2do Cuatrimestre/clase07-08ejercicio2.c
@@ -4,22 +4,94 @@
 #include <string.h>
 #include <ctype.h>
 #include <math.h>
-#define N 10
+#define N 5
 
 void CargarMatriz(int [N][N]);
 void MostrarMatriz(int [N][N]);
+int Mayor(int [N][N]);
+void Encontrar(int [N][N], int);
 
 int main (void)
 {
-  int mat[N][N];
-  srand(time(NULL));
-  CargarMatriz(mat);
-  MostrarMatriz(mat);
 
-  return 0;
+    int mat[N][N], max, aux;
+    srand(time (NULL));
+    CargarMatriz(mat);
+    MostrarMatriz(mat);
+    Mayor(mat);
+    max = Mayor(mat);
+    Encontrar(mat, max);
+
+    
+
+    printf("El mayor es %d y se repite %d veces", max, aux);
+    return 0;
 }
 
 void CargarMatriz(int mat[N][N])
 {
+    int CS=15, Ci=1, i, j;
 
+    for(i=0;i<N;i++)
+    {
+        for(j=0;j<N;j++)
+        {
+            mat[i][j]=rand()%(CS - Ci + 1) + Ci;
+        }
+    }
+}
+
+void MostrarMatriz(int mat[N][N])
+{
+      int i, j;
+
+    for(i=0;i<N;i++)
+    {
+        for(j=0;j<N;j++)
+        {
+            printf("%3d", mat[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+int Mayor (int mat[N][N]){
+    int i, j, max, fil=0, col=0;
+    max = mat[0][0];
+    for (i=0; i<N; i++)
+    {
+        for (j=0; j<N; j++)
+        {
+            if (mat[i][j]>max)
+            {
+                max = mat[i][j];
+                fil = i;
+                col = j;
+            }
+        }
+    }
+
+    printf("El mayor es: %d y su posicion es %d,%d\n", max, fil, col);
+    return max;
+}
+
+void Encontrar(int mat[N][N], int max)
+{
+    int i, j, aux=0;
+    for(i=0;i<N;i++)
+    {
+        for(j=0;j<N;j++)
+        {   
+            if(max > mat[i][j])
+            {
+              aux = aux + 1;
+              
+            }
+            
+        }
+        
+    }
+    
+     //printf("El mayor %3d se repite %d veces", max, aux);
+    
 }
--- informaticaAplicada/programacion1practicasDeC/ejercicio1y2Clase07-8.c
+++ informaticaAplicada/programacion1practicasDeC/ejercicio1y2Clase07-8.c
@@ -9,26 +9,25 @@
 void CargarMatriz(int [N][N]);
 void MostrarMatriz(int [N][N]);
 void Mayor(int [N][N]);
-int Encontrar(int [N][N], int);
+//void Encontrar(int [N][N], int);
 
 int main (void)
 {
-    int max;
 
-    int mat[N][N]={0};
+    int mat[N][N];
     srand(time (NULL));
     CargarMatriz(mat);
     MostrarMatriz(mat);
     Mayor(mat);
-    Encontrar(mat, max);
+    //Encontrar(mat, max);
 
-    printf("El mayor numero es: %3d", max);
+
     return 0;
 }
 
 void CargarMatriz(int mat[N][N])
 {
-    int CS=100, Ci=1, i, j;
+    int CS=30, Ci=1, i, j;
 
     for(i=0;i<N;i++)
     {
@@ -53,37 +52,36 @@ void MostrarMatriz(int mat[N][N])
     }
 }
 
-void Mayor (int m[N][N]){
+void Mayor (int mat[N][N]){
     int i, j, max, fil=0, col=0;
-    max = m[0][0];
+    max = mat[0][0];
     for (i=0; i<N; i++)
     {
         for (j=0; j<N; j++)
         {
-            if (m[i][j]>max)
+            if (mat[i][j]>max)
             {
-                max = m[i][j];
+                max = mat[i][j];
                 fil = i;
                 col = j;
             }
         }
     }
+
+    printf("El mayor es: %d y su posicion es %d,%d\n", max, fil, col);
 }
 
-int Encontrar(int mat[N][N], int max)
-{
-    int i, j;
-    for(i=0;i<N;i++)
-    {
-        for(j=0;j<N;j++)
-        {
-            if (mat[i][j]==max)
-            {
-                printf("El mayor esta en la posicion: %3d", mat[i][j]);
-            }
-            
-        }
-        printf("\n");
-    }
-    return 0;
-}
+// void Encontrar(int mat[N][N], int max)
+// {
+//     int i, j, aux=0, fil=0, col=0;
+//     for(i=0;i<N;i++)
+//     {
+//         for(j=0;j<N;j++)
+//         {   
+//             if(max > aux)
+//             aux = max;
+//         }
+        
+//     }
+//     printf("El mayor %3d esta en la posicion: %d%d", max, fil,col);
+// }
