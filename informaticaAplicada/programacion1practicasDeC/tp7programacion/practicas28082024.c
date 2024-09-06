c8284e1ceb2b0ce45ea625aa34998bd7f0d97ff7
Rafael <mg.rafaeld@gmail.com>
2024-09-06 18:59:41 -0300

    avanzando con los struct

--- informaticaAplicada/programacion1practicasDeC/tp7programacion/practicas28082024.c
+++ informaticaAplicada/programacion1practicasDeC/tp7programacion/practicas28082024.c
@@ -4,31 +4,24 @@
 #include <string.h>
 #include <ctype.h>
 #include <math.h>
+#define long 50
 
-// int main()
-// {
-//   struct Persona
-//   {
-//     int cod;
-//     char Apellido;
-
-//   };
-
-//   typedef struct Persona cliente;
-  
-// }
-
-struct Persona
+typedef struct
 {
   char nombre [50];
   int edad;
-};
+}Persona;
 
-void CargaDatos(struct Persona person1*,char[50]*,int*);
+void CargaDatos(Persona[]);
 
 int main()
 {
-  CargaDatos(Persona, &nombre);
+  Persona pers[long];
+
+  CargaDatos(pers);
+  printf("El nombre de la persona es, %s\n", pers[0].nombre);
+  printf("La edad de la persona es, %d\n", pers[1].edad);
+
   //struct Persona person1;
 
   //strcpy(person1.nombre, "Juan");
@@ -36,13 +29,14 @@ int main()
 
   //printf("Nombre: %s\n", person1.nombre);
   //printf("Edad: %d\n", person1.edad);
+
+  return 0;
 }
 
-void CargaDatos(Persona,char *nombre[50],int *edad)
+void CargaDatos(Persona pers[long])
 {
-  
-
-  printf("Ingrese el nombre de la persona");
-
-
+  printf("Ingrese el nombre de la persona: \n");
+  gets(pers[0].nombre);
+  printf("Ingrese la edad de la persona: \n");
+  scanf("%d", &pers[1].edad);
 }
