371aab116f5576277582b86bcc1b2d5228803eec
Rafael <mg.rafaeld@gmail.com>
2024-10-05 22:57:46 -0300

    practica de excel y de prog

--- informaticaAplicada/programacion1practicasDeC/practicasTP5programacion/funciones/tp5level1ejercicio5.c
+++ informaticaAplicada/programacion1practicasDeC/practicasTP5programacion/funciones/tp5level1ejercicio5.c
@@ -1,3 +1,10 @@
 #include <stdio.h>
+#include <string.h>
 #define P printf
 #define S scanf
+
+
+
+//vectores - funciones - while/dowhile
+/*llenar el vector de forma automatica y secuencial con los numeros primos hasta el enesimo numero
+Este enesimo numero debe ser indicado por el usuario*/
